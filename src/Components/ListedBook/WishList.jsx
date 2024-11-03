7f5c15ae24804c6aef54226430927a50a264b6ef
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-11-03 12:21:07 +0600

    sort updated

--- src/Components/ListedBook/WishList.jsx
+++ src/Components/ListedBook/WishList.jsx
@@ -4,6 +4,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { MyContext } from '../../utils/MyProvider'
 import SingleWishBookCard from './SingleWishBookCard';
+import SortComponent from './SortComponent';
 
 export default function WishList() {
     const { getStoreWishList} = useContext(MyContext);
@@ -15,6 +16,9 @@ export default function WishList() {
     }, []);
 
   return (
-    <div className='flex flex-col gap-4'>{wishList?.map((book)=><SingleWishBookCard key={book.bookId} book={book}/>)}</div>
+   <div className='flex flex-col gap-4'>
+    <SortComponent readList={wishList} setReadList={setWishList}/>
+     <div className='flex flex-col gap-4'>{wishList?.map((book)=><SingleWishBookCard key={book.bookId} book={book}/>)}</div>
+   </div>
   )
 }
